5c0499219187687d1fa4e32cf21c324da08e0d6f
David <dcicero493@gmail.com>
2020-07-05 16:02:33 -0300

    inserir imagens no banco

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace InserirImagemBanco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(26, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::InserirImagemBanco.Properties.Resources.user_default;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 219);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Selecionar Imagem";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(76, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 42);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SALVAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 442);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InserirImagemBanco
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;//bitmap em classe global, ele é uma classe que trabalha com os pixels de uma imagem.
+
+        const string StringConexao = @"Data Source=DESKTOP-MCQJPQB;Initial Catalog=master;Integrated Security=True";//a minha tabela foi feita na pasta master.
+
+        SqlConnection conexao = new SqlConnection(StringConexao);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("select * from tbimagem where numero = @numero", conexao);
+            
+            SqlParameter numero = new SqlParameter("@numero", SqlDbType.Int);
+            
+            numero.Value = textBox2.Text;
+
+            comando.Parameters.Add(numero);
+
+            try
+            {
+                conexao.Open();
+
+                SqlDataReader reader = comando.ExecuteReader();//aqui foi criado um objeto o datareader para ler as linhas da tabela e buscar os dados que ele acabou de ler.
+
+                reader.Read();
+
+                if (reader.HasRows)//quando o reader fizer a leitura se tiver alguma linha ou seja tiver algum conteudo dentro dele, pegamos e jogamos no formulario.
+                {
+                    textBox1.Text = reader[1].ToString();//esta parte vai pegar o que tem na tabela e mostrar na tela(formulario) o [1] representa a linha da tabela onde esta o nome como na tabela tem 3 linha e a primeira é a de codigo que corresponde ao [0] que nao vou usar aqui entao colocamos o [1] que é onde esta a linha nome.
+                    byte[] imagem = (byte[])(reader[2]);//aqui vamos no campo [2] da tabela do banco onde esta a imagem.
+
+                    if (imagem == null)//esta condição é para se o array onde esta a imagem for igual a nulo ele nao vai mostrar nada da picturebox nada.
+                        pictureBox1.Image = null;
+                    else//caso contrario vamos fazer mais um objeto memorystream que contem a imagem, ele é responsavel por guardar a imagem na memoria.
+                    {
+                        MemoryStream memory = new MemoryStream(imagem);//aqui no objeto colocamos (imagem) que é a array do tipo byte que contem os dados da imagem que estamos buscando.
+                        pictureBox1.Image = Image.FromStream(memory);//aqui vamos mandar a imagem reculperada para a picturebox
+                    }
+                }
+
+                //conexao.Close(); no video o cara apagou o finally onde fecha a conexao mas eu resolvi manter no finally se der errado vou apagar o finally e deixo essa parte para ver se o erro é aqui.
+
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Todos os codigos neste butao é para pegar buscar a imagem e coloca-la no picturebox para exibir para o usuario.
+
+            //openFileDialog serve para abrir a pasta de arquivos do pc.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)//Quando o usuario selecionar o seu arquivo e clicar em OK vem para ca.
+            {
+                string nome = openFileDialog1.FileName;//para pegar o nome do arquivo escolhido.
+
+                bmp = new Bitmap(nome);
+
+                pictureBox1.Image = bmp;//aqui vamos pegar o nome da imagem e colocalo no picturebox para aparecer a imagem no picturebox sem necessidade de colocar a imagem antes.
+            }
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Agora vamos salvar o arquivo selecionado.
+
+            MemoryStream memory = new MemoryStream();//O memorystream é um objeto que serve para guardar dados na memoria.
+
+            bmp.Save(memory, ImageFormat.Bmp);
+
+            byte[] foto = memory.ToArray();
+
+            SqlCommand comando = new SqlCommand("INSERT INTO tbimagem (nome, imagem, numero) VALUES(@nome, @imagem, @numero)", conexao); //O que esta dentro dos parenteses é a sintaxe para salvar as imagens no banco.
+
+            SqlParameter nome = new SqlParameter("@nome", SqlDbType.VarChar);
+            SqlParameter imagem = new SqlParameter("imagem", SqlDbType.Binary);
+            SqlParameter numero = new SqlParameter("numero", SqlDbType.Int);
+            nome.Value = textBox1.Text;
+            imagem.Value = foto;
+            numero.Value = textBox2.Text;
+
+            comando.Parameters.Add(nome);
+            comando.Parameters.Add(imagem);
+            comando.Parameters.Add(numero);
+
+            try
+            {
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Imagem SALVA no banco de dados com SUCESSO!");
+
+                textBox1.Text = "";
+                pictureBox1.Image = null;
+
+            }
+            catch (Exception E)
+            {
+
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+    }
+}
